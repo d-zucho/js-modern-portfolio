2e5cf2bf7a66348fb525e2e96ba313039f8e1a48
d-zucho <danny.nico.dev@outlook.com>
2023-02-01 22:53:45 -0800

    add header and background with framer motion animation

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,9 +1,19 @@
-'use client';
+'use client'
+
+import { motion } from 'framer-motion'
+
+import styles from '../styles'
+import { slideIn, staggetContainer, textVariant } from '../utils/motion'
 
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggetContainer}
+      initial='hidden'
+      whileInView='show' // whileInView is a custom hook that detects when the element is in the viewport
+      viewport={{ once: false, amount: 0.25 }} //states how much and how often the element should be in the viewport to trigger the animation
+    ></motion.div>
   </section>
-);
+)
 
-export default Hero;
+export default Hero
